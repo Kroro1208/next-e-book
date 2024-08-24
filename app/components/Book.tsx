19b42f78dea8cdd559071c0b9985126bc5160fcc
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-08-24 15:09:33 +0900

    Homeページのレスポンシブ対応した

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -63,61 +63,64 @@ const Book = ({ book, isPurchased }: Props) => {
     }
     return (
         <>
-            <style jsx global>
-                {`
-                    @keyframes fadeIn {
-                        from {
-                            opacity: 0;
-                            transform: scale(0.9);
-                        }
-                        to {
-                            opacity: 1;
-                            transform: scale(1);
-                        }
-                    }
-                    .modal {
-                        animation: fadeIn 0.3s ease-out forwards;
-                    }
-                `}
-            </style>
+            <style jsx global>{`
+            @keyframes fadeIn {
+              from {
+                opacity: 0;
+                transform: scale(0.9);
+              }
+              to {
+                opacity: 1;
+                transform: scale(1);
+              }
+            }
+            .modal {
+              animation: fadeIn 0.3s ease-out forwards;
+            }
+          `}</style>
 
             <div className="flex flex-col items-center m-4">
                 <a
                     onClick={handlePurchase}
-                    className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none"
+                    className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none max-w-sm w-full"
                 >
-                    <Image
-                        priority
-                        src={book.thumbnail.url}
-                        alt={book.title}
-                        width={550}
-                        height={550}
-                        className="rounded-xl h-80"
-                    />
+                    <div className="relative w-full max-h-44 aspect-square">
+                        <Image
+                            priority
+                            src={book.thumbnail.url}
+                            alt={book.title}
+                            layout="fill"
+                            objectFit="cover"
+                            className="rounded-t-xl"
+                        />
+                    </div>
                     <div className="px-4 py-4 bg-slate-100 rounded-b-md">
-                        <h2 className="text-lg font-semibold">{book.title}</h2>
-                        <p className="mt-2 text-md text-slate-700">値段：{book.price}円</p>
+                        <h2 className="text-lg sm:text-xl font-semibold">{book.title}</h2>
+                        <p className="mt-2 text-sm sm:text-md text-slate-700">値段：{book.price}円</p>
                     </div>
                 </a>
                 {/* modal */}
-                {
-                    showModal &&
-                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-                        <div className="bg-white p-8 rounded-lg">
-                            <h3 className="text-xl mb-4 text-center">購入しますか？</h3>
-                            <button
-                                onClick={handlePurchaseConfirm}
-                                className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-4 rounded-md mr-4">
-                                購入する
-                            </button>
-                            <button
-                                onClick={handleCancel}
-                                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-4 rounded-md">
-                                キャンセル
-                            </button>
+                {showModal && (
+                    <div className="fixed inset-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal z-50">
+                        <div className="bg-white p-6 sm:p-8 rounded-lg max-w-xs sm:max-w-sm w-full mx-4">
+                            <h3 className="text-lg sm:text-xl mb-4 text-center">購入しますか？</h3>
+                            <div className="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0 sm:space-x-4">
+                                <button
+                                    onClick={handlePurchaseConfirm}
+                                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md"
+                                >
+                                    購入する
+                                </button>
+                                <button
+                                    onClick={handleCancel}
+                                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-md"
+                                >
+                                    キャンセル
+                                </button>
+                            </div>
                         </div>
                     </div>
-                }
+                )}
             </div>
         </>
     );
